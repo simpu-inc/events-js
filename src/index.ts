39ec9f0327bb5a69f13751ed31f50caef4759e75
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2023-01-14 12:23:36 +0100

    refactor(index.ts): refactor to use class

--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,96 @@
-/**
- * Insipred by https://github.com/engage-so/engage-js/blob/master/packages/core/index.js
- */
 import fetch from 'cross-fetch';
 import { SimpuEventsError } from './error';
 
-const baseUrl = 'https://events.simpu.sh';
+export class Simpu {
+  key?: string;
+  baseUrl = 'https://events.simpu.sh';
 
-const options: { [key: string]: string | null } = {
-  key: null,
-};
+  constructor(key?: string) {
+    this.key = key;
+  }
 
-async function _request(
-  url: string,
-  params: object,
-  method: 'POST' | 'GET' | 'DELETE' | 'PUT' | 'PATCH'
-) {
-  try {
-    if (!params) {
-      params = {};
-    }
-    const response = await fetch(url, {
-      method,
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        Authorization: options.key ?? '',
-      },
-      body: JSON.stringify(params),
-    });
-    const body = await response.json();
-    let error = 'API connection error';
-    if (!response.ok) {
-      if (body && body.error) {
-        error = body.error;
+  async _request(
+    url: string,
+    params: object,
+    method: 'POST' | 'GET' | 'DELETE' | 'PUT' | 'PATCH'
+  ) {
+    try {
+      if (!params) {
+        params = {};
       }
-      return { error };
+      const response = await fetch(url, {
+        method,
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+          Authorization: this.key ?? '',
+        },
+        body: JSON.stringify(params),
+      });
+      const body = await response.json();
+      let error = 'API connection error';
+      if (!response.ok) {
+        if (body && body.error) {
+          error = body.error;
+        }
+        return { error };
+      }
+      return body;
+    } catch (e) {
+      return { error: 'API connection error' };
     }
-    return body;
-  } catch (e) {
-    return { error: 'API connection error' };
+  }
+
+  request(
+    url: string,
+    params: object,
+    method: 'POST' | 'GET' | 'DELETE' | 'PUT' | 'PATCH'
+  ) {
+    return this._request(`${this.baseUrl}${url}`, params, method);
+  }
+
+  init(o: string | { key: string }) {
+    if (!o) {
+      throw new SimpuEventsError('You need to pass in your API key.');
+    }
+    if (typeof o === 'string') {
+      this.key = o;
+      return;
+    }
+
+    if (!o.key) {
+      throw new SimpuEventsError('`key` missing in object.');
+    }
+    if (o.key) {
+      this.key = `${o.key}`;
+    }
+  }
+
+  // Identify users
+  async identify(o: { user_id: string; traits: object }) {
+    if (!o) {
+      throw new SimpuEventsError(
+        'You need to pass an object with at least a user id'
+      );
+    }
+    if (!o.user_id) {
+      throw new SimpuEventsError('User id is required.');
+    }
+    if (!o.traits) {
+      throw new SimpuEventsError('User trait is required.');
+    }
+
+    return this.request('/users', o, 'POST');
+  }
+
+  // Track events
+  async track(o: { user_id: string; event_key: string; properties?: object }) {
+    if (!o.user_id) {
+      throw new SimpuEventsError('User id is required.');
+    }
+    if (!o.event_key) {
+      throw new SimpuEventsError('Event name is required.');
+    }
+
+    return this.request('/event', o, 'POST');
   }
 }
-
-// Alias of _request method
-const request = (
-  url: string,
-  params: object,
-  method: 'POST' | 'GET' | 'DELETE' | 'PUT' | 'PATCH'
-) => {
-  return _request(`${baseUrl}${url}`, params, method);
-};
-
-// Initialize SDK and setup API Authorization
-export const init = (o: { key: string }) => {
-  if (!o) {
-    throw new SimpuEventsError('You need to pass in your API key.');
-  }
-  if (typeof o === 'string') {
-    options.key = o;
-    return;
-  }
-
-  if (!o.key) {
-    throw new SimpuEventsError('`key` missing in object.');
-  }
-  if (o.key) {
-    options.key = `${o.key}`;
-  }
-};
-
-// Identify users
-export const identify = async (o: { user_id: string; traits: object }) => {
-  if (!o) {
-    throw new SimpuEventsError(
-      'You need to pass an object with at least a user id'
-    );
-  }
-  if (!o.user_id) {
-    throw new SimpuEventsError('User id is required.');
-  }
-  if (!o.traits) {
-    throw new SimpuEventsError('User trait is required.');
-  }
-
-  return request('/users', o, 'POST');
-};
-
-// Track events
-export const track = async (o: {
-  user_id: string;
-  event_key: string;
-  properties?: object;
-}) => {
-  if (!o.user_id) {
-    throw new SimpuEventsError('User id is required.');
-  }
-  if (!o.event_key) {
-    throw new SimpuEventsError('Event name is required.');
-  }
-
-  return request('/event', o, 'POST');
-};
